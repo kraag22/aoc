872cbc9ceb49218c782f4716db0311f651de675d
Martin Bartušek <martinbartusek@gmail.com>
2022-02-21 17:00:26 +0100

    day 16 - allow retrieval of data from multi-block UInt

--- src/main/kotlin/advent/day16/Sixteenth.kt
+++ src/main/kotlin/advent/day16/Sixteenth.kt
@@ -3,9 +3,9 @@ package advent.day16
 import advent.Base
 import kotlin.math.ceil
 
+@OptIn(ExperimentalUnsignedTypes::class)
 class Sixteenth : Base() {
 
-    @kotlin.ExperimentalUnsignedTypes
     fun parse(hex: String): UIntArray {
         val size = ceil(hex.length / 8f).toInt()
         val array = UIntArray(size)
@@ -15,12 +15,38 @@ class Sixteenth : Base() {
         return array
     }
 
+    fun readHeader(data: UIntArray, position: Int) {
+        val version = data[0].getBits(position, 3)
+        val typeId = data[0].getBits(position + 3, 3)
+        println("version: ${version.toString(2)}, $version")
+        println("typeId: ${typeId.toString(2)}, $typeId")
+
+    }
 }
 
 fun String.hexToUInt() = toUInt(16)
 
 fun UInt.takeFirstBits(bitCount: Int) = this shr (32 - bitCount)
-fun UInt.getBites(at: Int, bitCount: Int): UInt {
+fun UInt.getBits(at: Int, bitCount: Int): UInt {
+    check(at + bitCount <= UInt.SIZE_BITS) { "Cannot access bits outside UInt at: $at, bitCount: $bitCount" }
     val shifted = this shl at
     return shifted.takeFirstBits(bitCount)
+}
+
+fun UIntArray.getBits(at: Int, bitCount: Int): UInt {
+    val blockSize = UInt.SIZE_BITS
+    check(bitCount <= blockSize) { "bitCount is greater than size of UInt: $blockSize" }
+    check(at + bitCount <= size * blockSize) { "Trying to get data outside range at: $at, bitCount: $bitCount" }
+
+    val blockNo = at.floorDiv(blockSize)
+    val blockAt = at.mod(blockSize)
+
+    return if (blockAt + bitCount <= blockSize) {
+        this[blockNo].getBits(blockAt, bitCount)
+    } else {
+        val otherBlockBitCount = (blockAt + bitCount).mod(blockSize)
+        val firstPart = this[blockNo].getBits(blockAt, blockSize - blockAt)
+        val secondPart = this[blockNo + 1].getBits(0, otherBlockBitCount)
+        return (firstPart shl otherBlockBitCount) or secondPart
+    }
 }
--- src/test/kotlin/advent/day16/SixteenthTest.kt
+++ src/test/kotlin/advent/day16/SixteenthTest.kt
@@ -1,16 +1,46 @@
 package advent.day16
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 
+@OptIn(ExperimentalUnsignedTypes::class)
 internal class SixteenthTest {
     @Test
     fun `operations should work`() {
         assertThat("D2".hexToUInt()).isEqualTo(0b11010010.toUInt())
-        val bites = 0b11010010111111100010100000000000.toUInt()
+        val bits = 0b11010010111111100010100000000000.toUInt()
 
-        assertThat(bites.takeFirstBits(3)).isEqualTo(0b110.toUInt())
-        assertThat(bites.getBites(2, 2)).isEqualTo(0b01.toUInt())
+        assertThat(bits.takeFirstBits(3)).isEqualTo(0b110.toUInt())
+        assertThat(bits.getBits(2, 2)).isEqualTo(0b01.toUInt())
+        assertThatThrownBy { bits.getBits(28, 6) }.isInstanceOf(IllegalStateException::class.java)
+    }
+
+    @Test
+    fun `operations on array in one block should work`() {
+        val data = UIntArray(1) { 0b11010010111111100010100000000000.toUInt() }
+
+        assertThat(data.getBits(0, 3)).isEqualTo(0b110.toUInt())
+        assertThat(data.getBits(2, 2)).isEqualTo(0b01.toUInt())
+        assertThatThrownBy { data.getBits(28, 6) }.isInstanceOf(IllegalStateException::class.java)
+        assertThatThrownBy { data.getBits(0, 34) }.isInstanceOf(IllegalStateException::class.java)
+    }
+
+    @Test
+    fun `operations on array in multiple blocks should work`() {
+        val data = UIntArray(2)
+        data[0] = 0b11010010111111100010100000000010.toUInt()
+        data[1] = 0b11010010111111100010100000000000.toUInt()
+
+        assertThat(data.getBits(32, 3)).isEqualTo(0b110.toUInt())
+        assertThat(data.getBits(0, 32)).isEqualTo(data[0])
+        assertThat(data.getBits(32, 32)).isEqualTo(data[1])
+
+        assertThat(data.getBits(30, 4)).isEqualTo(0b1011.toUInt())
+        assertThat(data.getBits(16, 32)).isEqualTo(0b101000000000101101001011111110.toUInt())
+
+        assertThatThrownBy { data.getBits(60, 6) }.isInstanceOf(IllegalStateException::class.java)
+        assertThatThrownBy { data.getBits(0, 34) }.isInstanceOf(IllegalStateException::class.java)
     }
 
     @Test
@@ -37,9 +67,17 @@ internal class SixteenthTest {
         val s = Sixteenth()
         val bites = s.parse("D2FE28").first()
         val version = bites.takeFirstBits(3)
-        val typeId = bites.getBites(3, 3)
+        val typeId = bites.getBits(3, 3)
 
         assertThat(version).isEqualTo(6.toUInt())
         assertThat(typeId).isEqualTo(4.toUInt())
+    }
+
+    @Test
+    fun `decode example data`() {
+        val s = Sixteenth()
+        val arr = s.parse("D2FE28")
+
+        s.readHeader(arr, 0)
     }
 }
