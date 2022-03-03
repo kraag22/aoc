9307b95cefc5e8e32692fd332406d5fe50abe7c9
Martin Bartušek <martinbartusek@gmail.com>
2022-03-03 16:12:56 +0100

    day 18 - add explode()

--- src/main/kotlin/advent/day18/Eighteenth.kt
+++ src/main/kotlin/advent/day18/Eighteenth.kt
@@ -3,9 +3,11 @@ package advent.day18
 import advent.Base
 
 class Eighteenth : Base() {
-    fun parse(input: String): Snailfish {
+    fun parse(input: String, previous: Snailfish? = null): Snailfish {
+        val snailfish = Snailfish(previous)
         if (!input.contains(',')) {
-            return Snailfish(input.toLong(), null)
+            snailfish.value = input.toLong()
+            return snailfish
         }
 
         var bracketDepth = 0
@@ -17,34 +19,133 @@ class Eighteenth : Base() {
                     if (bracketDepth == 1) {
                         val left = input.drop(1).take(idx - 1)
                         val right = input.drop(idx + 1).dropLast(1)
-                        return Snailfish(Pair(parse(left), parse(right)), null)
+                        snailfish.pair = Pair(parse(left, snailfish), parse(right, snailfish))
+                        return snailfish
                     }
                 }
-                else -> {
-
-                }
+                else -> {}
             }
         }
 
-        throw Exception("shouldnt end like this")
+        throw Exception("Unexpected end of parsing")
     }
 }
 
 class Snailfish {
     var value: Long? = null
     var pair: Pair<Snailfish, Snailfish>? = null
-    var previous: Snailfish? = null
+    var parent: Snailfish? = null
+
+    constructor(_previous: Snailfish?) {
+        value = null
+        pair = null
+        parent = _previous
+    }
 
     constructor(_value: Long, _previous: Snailfish?) {
         value = _value
         pair = null
-        previous = _previous
+        parent = _previous
     }
 
     constructor(_pair: Pair<Snailfish, Snailfish>, _previous: Snailfish?) {
         value = null
         pair = _pair
-        previous = _previous
+        parent = _previous
+    }
+
+    fun setValue(_v: Long) {
+        value = _v
+        pair = null
+    }
+
+    private fun increaseValue(increase: Long) {
+        value = value!! + increase
+    }
+
+    fun getLevel(baseLevel: Int? = null): Int {
+        val level = baseLevel ?: 0
+        return if (isSingle()) {
+            level
+        } else {
+            val p = pair
+            check(p != null) { "Missing pair for nonSingle snailfish" }
+            maxOf(p.first.getLevel(level + 1), p.second.getLevel(level + 1))
+        }
+    }
+
+    fun explode(): Boolean {
+        val toExplode = getExploding() ?: return false
+        val increaseLeft =
+            toExplode.pair?.first?.value ?: throw Exception("Value cannot be empty for fish: ${toExplode.print()}")
+        val increaseRight =
+            toExplode.pair?.second?.value ?: throw Exception("Value cannot be empty for fish: ${toExplode.print()}")
+
+        val leftNeighbour = toExplode.parent?.getNeighbour(goDown = true, goLeft = true, toExplode)
+        val rightNeighbour = toExplode.parent?.getNeighbour(goDown = true, goLeft = false, toExplode)
+
+        leftNeighbour?.increaseValue(increaseLeft)
+        rightNeighbour?.increaseValue(increaseRight)
+
+        toExplode.setValue(0)
+        return true
+    }
+
+    fun getExploding(baseLevel: Int? = null): Snailfish? {
+        val level = baseLevel ?: 1
+        check(level < 6) { "Level cannot be that high: $level fish: ${this.print()}" }
+        return if (isSingle()) {
+            null
+        } else {
+            val p = pair
+            check(p != null) { "Missing pair for nonSingle snailfish" }
+            if (level == 5) {
+                this
+            } else {
+                p.first.getExploding(level + 1) ?: p.second.getExploding(level + 1)
+            }
+        }
+    }
+
+    fun getNeighbour(goDown: Boolean, goLeft: Boolean, previous: Snailfish): Snailfish? {
+        return if (isSingle()) {
+            this
+        } else {
+            val res = computeNeighbours(goDown, goLeft, previous)
+            if (res == null) {
+                if (parent == null) {
+                    null
+                } else {
+                    parent!!.getNeighbour(true, goLeft, this)
+                }
+            } else {
+                res
+            }
+        }
+    }
+
+    private fun computeNeighbours(goDown: Boolean, goLeft: Boolean, previous: Snailfish): Snailfish? {
+        return if (goDown) {
+            if (goLeft) {
+                go(pair!!.first, goLeft, previous)
+            } else {
+                go(pair!!.second, goLeft, previous)
+            }
+        } else {
+            if (goLeft) {
+                go(pair!!.second, goLeft, previous) ?: go(pair!!.first, goLeft, previous)
+            } else {
+                go(pair!!.first, goLeft, previous) ?: go(pair!!.second, goLeft, previous)
+            }
+        }
+    }
+
+    private fun go(to: Snailfish, goLeft: Boolean, previous: Snailfish): Snailfish? {
+        return if (to == previous) {
+            null
+        } else {
+            to.getNeighbour(false, goLeft, this)
+        }
     }
 
     fun print(): String {
--- src/test/kotlin/advent/day18/EighteenthTest.kt
+++ src/test/kotlin/advent/day18/EighteenthTest.kt
@@ -14,11 +14,126 @@ internal class EighteenthTest {
             "[[[[1,2],[3,4]],[[5,6],[7,8]]],9]",
             "[[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]]",
             "[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]",
+            "[[[[1,3],[5,3]],[[1,3],[8,[2,3]]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]"
         )
         for (number in numbers) {
             assertThat(e.parse(number).print()).isEqualTo(number)
         }
+    }
 
+    @Test
+    fun `test levels`() {
+        val e = Eighteenth()
+        val numbers = listOf(
+            Pair("[1,2]", 1),
+            Pair("[9,[8,7]]", 2),
+            Pair("[[1,9],[8,5]]", 2),
+            Pair("[[[[1,2],[3,4]],[[5,6],[7,8]]],9]", 4),
+            Pair("[[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]]", 4),
+            Pair("[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]", 4),
+        )
+        for (it in numbers) {
+            assertThat(e.parse(it.first).getLevel())
+                .describedAs("Failed for snailfish: ${it.first}")
+                .isEqualTo(it.second)
+        }
+    }
 
+    @Test
+    fun `get exploding leaves`() {
+        val e = Eighteenth()
+        val numbers = listOf(
+            Pair("[1,2]", null),
+            Pair("[9,[8,7]]", null),
+            Pair("[[1,9],[8,5]]", null),
+            Pair("[[[[1,2],[3,4]],[[5,6],[7,8]]],9]", null),
+            Pair("[[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]]", null),
+            Pair("[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]", null),
+            Pair("[[[[[1,1],3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]", "[1,1]"),
+            Pair("[[[[1,3],[5,3]],[[1,3],[8,[2,3]]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]", "[2,3]"),
+            Pair("[[[[0,7],4],[7,[[8,4],9]]],[1,1]]", "[8,4]")
+        )
+        for (it in numbers) {
+            assertThat(e.parse(it.first).getExploding()?.print())
+                .describedAs("Failed for snailfish: ${it.first}")
+                .isEqualTo(it.second)
+        }
+    }
+
+    @Test
+    fun `get non-existent neighbour on left of {1,1}`() {
+        val e = Eighteenth()
+        val fish = e.parse("[[[[[1,1],3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]")
+        val exploding = fish.getExploding()
+        assertThat(exploding!!.parent?.getNeighbour(goDown = true, goLeft = false, exploding)?.print()).isEqualTo("3")
+        assertThat(
+            exploding!!.parent?.getNeighbour(goDown = true, goLeft = true, exploding)?.print()
+        ).isNull()
+    }
+
+    @Test
+    fun `get neighbours of {2,3}`() {
+        val e = Eighteenth()
+        val fish = e.parse("[[[[1,3],[5,3]],[[1,3],[8,[2,3]]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]")
+        val exploding = fish.getExploding()
+        assertThat(
+            exploding!!.parent?.getNeighbour(goDown = true, goLeft = false, exploding)?.print()
+        ).isEqualTo("4")
+        assertThat(
+            exploding!!.parent?.getNeighbour(goDown = true, goLeft = true, exploding)?.print()
+        ).isEqualTo("8")
+    }
+
+    @Test
+    fun `get neighbours of {8,4}`() {
+        val e = Eighteenth()
+        val fish = e.parse("[[[[0,7],4],[7,[[8,4],9]]],[1,1]]")
+        val exploding = fish.getExploding()
+        assertThat(
+            exploding!!.parent?.getNeighbour(goDown = true, goLeft = false, exploding)?.print()
+        ).isEqualTo("9")
+        assertThat(
+            exploding!!.parent?.getNeighbour(goDown = true, goLeft = true, exploding)?.print()
+        ).isEqualTo("7")
+    }
+
+    @Test
+    fun `test explosion 1`() {
+        val e = Eighteenth()
+        val fish = e.parse("[[[[[9,8],1],2],3],4]")
+        fish.explode()
+        assertThat(fish.print()).isEqualTo("[[[[0,9],2],3],4]")
+    }
+
+    @Test
+    fun `test explosion 2`() {
+        val e = Eighteenth()
+        val fish = e.parse("[7,[6,[5,[4,[3,2]]]]]")
+        fish.explode()
+        assertThat(fish.print()).isEqualTo("[7,[6,[5,[7,0]]]]")
+    }
+
+    @Test
+    fun `test explosion 3`() {
+        val e = Eighteenth()
+        val fish = e.parse("[[6,[5,[4,[3,2]]]],1]")
+        fish.explode()
+        assertThat(fish.print()).isEqualTo("[[6,[5,[7,0]]],3]")
+    }
+
+    @Test
+    fun `test explosion 4`() {
+        val e = Eighteenth()
+        val fish = e.parse("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]")
+        fish.explode()
+        assertThat(fish.print()).isEqualTo("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]")
+    }
+
+    @Test
+    fun `test explosion 5`() {
+        val e = Eighteenth()
+        val fish = e.parse("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]")
+        fish.explode()
+        assertThat(fish.print()).isEqualTo("[[3,[2,[8,0]]],[9,[5,[7,0]]]]")
     }
 }
