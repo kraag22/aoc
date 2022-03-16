a73a4a4b17db16536dae6957ac9ea4f77c0ab7b6
Martin Bartušek <martinbartusek@gmail.com>
2022-03-16 22:49:35 +0100

    day 20 - bugged first part

---
+++ src/main/kotlin/advent/day20/Twentieth.kt
@@ -0,0 +1,97 @@
+package advent.day20
+
+import advent.Base
+
+class Twentieth : Base() {
+    var enhancement = listOf<Char>()
+    var inputImage = mutableListOf<List<Char>>()
+    override fun parseAndStore(lines: List<String>) {
+        enhancement = lines.first().toList()
+
+        lines.drop(2).forEach {
+            inputImage.add(it.toList())
+        }
+    }
+
+    fun enhanceImage(): List<List<Char>> {
+        val outputImage = mutableListOf<List<Char>>()
+        for (y in (-4..inputImage.yMax + 3)) {
+            val line = mutableListOf<Char>()
+            for (x in (-4..inputImage.xMax + 3)) {
+                line.add(computeOutputPoint(x, y))
+            }
+            outputImage.add(line)
+        }
+        return outputImage
+    }
+
+    fun twiceEnhance(): List<List<Char>> {
+        inputImage = enhanceImage().toMutableList()
+        return enhanceImage()
+    }
+
+    fun computeOutputPoint(x: Int, y: Int): Char {
+        val idx = getEnhancementIndex(x, y)
+        return enhancement[idx]
+    }
+
+    fun getInputPoint(x: Int, y: Int): Char {
+        val outOfBoundsX = x < 0 || x >= inputImage.xMax
+        val outOfBoundsY = y < 0 || y >= inputImage.yMax
+        if (outOfBoundsX || outOfBoundsY) {
+            return '.'
+        }
+
+        return inputImage[y][x]
+    }
+
+    fun getEnhancementIndex(x: Int, y: Int): Int {
+        val coords = listOf(
+            Pair(x - 1, y - 1),
+            Pair(x, y - 1),
+            Pair(x + 1, y - 1),
+            Pair(x - 1, y),
+            Pair(x, y),
+            Pair(x + 1, y),
+            Pair(x - 1, y + 1),
+            Pair(x, y + 1),
+            Pair(x + 1, y + 1),
+        )
+
+        return coords
+            .map { getInputPoint(it.x, it.y) }
+            .fold("") { acc, letter -> acc + parseLetter(letter) }
+            .toInt(2)
+    }
+
+    private fun parseLetter(letter: Char) = when (letter) {
+        '#' -> "1"
+        '.' -> "0"
+        else -> error("Cannot contain letter: $letter")
+    }
+}
+
+val Pair<Int, Int>.x get() = first
+val Pair<Int, Int>.y get() = second
+
+val List<List<Char>>.xMax get() = first().size
+val List<List<Char>>.yMax get() = size
+
+fun List<List<Char>>.print() {
+    for (y in (0 until yMax)) {
+        val line = get(y)
+        for (x in (0 until xMax)) {
+            print(line[x])
+        }
+        println()
+    }
+    println()
+}
+
+fun List<List<Char>>.countHashes(): Int {
+    var sum = 0
+    for (y in (0 until yMax)) {
+        sum += get(y).filter { it == '#' }.size
+    }
+    return sum
+}
---
+++ src/test/kotlin/advent/day20/TwentiethTest.kt
@@ -0,0 +1,104 @@
+package advent.day20
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class TwentiethTest {
+    @Test
+    fun `try to parse example data`() {
+        val t = Twentieth()
+        t.extract("/20_example.txt")
+
+        assertThat(t.enhancement).hasSize(512)
+        assertThat(t.inputImage).hasSize(5)
+        assertThat(t.inputImage.first()).hasSize(5)
+    }
+
+    @Test
+    fun `getting points from inputImage works`() {
+        val t = Twentieth()
+        t.extract("/20_example.txt")
+
+        assertThat(t.getInputPoint(3, 0)).isEqualTo('#')
+        assertThat(t.getInputPoint(-1, 3)).isEqualTo('.')
+        assertThat(t.getInputPoint(100, 3)).isEqualTo('.')
+        assertThat(t.getInputPoint(2, -1)).isEqualTo('.')
+        assertThat(t.getInputPoint(2, 100)).isEqualTo('.')
+        assertThat(t.getInputPoint(0, 1)).isEqualTo('#')
+        assertThat(t.getInputPoint(1, 2)).isEqualTo('#')
+        assertThat(t.getInputPoint(1, 1)).isEqualTo('.')
+    }
+
+    @Test
+    fun `computing enhancement index`() {
+        val t = Twentieth()
+        t.extract("/20_example.txt")
+
+        assertThat(t.getEnhancementIndex(2, 2)).isEqualTo(34)
+        assertThat(t.getEnhancementIndex(0, 0)).isEqualTo(18)
+    }
+
+    @Test
+    fun `computing output point`() {
+        val t = Twentieth()
+        t.extract("/20_example.txt")
+
+        assertThat(t.computeOutputPoint(2, 2)).isEqualTo('#')
+        assertThat(t.computeOutputPoint(0, 0)).isEqualTo('.')
+    }
+
+    @Test
+    fun `twice enhance example input image`() {
+        val t = Twentieth()
+        t.extract("/20_example.txt")
+
+        val enhancedImage = t.enhanceImage()
+
+        assertThat(enhancedImage[0]).isEqualTo(".##.##.".toList())
+        assertThat(enhancedImage[1]).isEqualTo("#..#.#.".toList())
+        assertThat(enhancedImage[2]).isEqualTo("##.#..#".toList())
+        assertThat(enhancedImage[6]).isEqualTo("...#.#.".toList())
+
+        t.inputImage = enhancedImage.toMutableList()
+        val twiceEnhancedImage = t.enhanceImage()
+
+        val expected = listOf(
+            ".......#.",
+            ".#..#.#..",
+            "#.#...###",
+            "#...##.#.",
+            "#.....#.#",
+            ".#.#####.",
+            "..#.#####",
+            "...##.##.",
+            "....###.."
+        )
+        expected.forEachIndexed { index, s ->
+            assertThat(twiceEnhancedImage.get(index)).isEqualTo(s.toList())
+        }
+
+        assertThat(twiceEnhancedImage.countHashes()).isEqualTo(35)
+    }
+
+    @Test
+    fun `twice enhance works on example data`() {
+        val t = Twentieth()
+        t.extract("/20_example.txt")
+
+        val enhancedImage = t.twiceEnhance()
+        assertThat(enhancedImage.countHashes()).isEqualTo(35)
+
+    }
+
+    @Test
+    fun `compute hashes on enhanced input data`() {
+        val t = Twentieth()
+        t.extract("/20.txt")
+
+        val enhancedImage = t.twiceEnhance()
+
+        enhancedImage.print()
+
+        assertThat(enhancedImage.countHashes()).isEqualTo(5288)
+    }
+}
